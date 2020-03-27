7fe684079e2b556f44dca713eaeb1f98dc3ae961
Mikah Mathews <mikah@outofband.org>
2020-03-27 15:50:57 -0700

    It will add working buttons, working branch, and fix ID names.

--- js/scripts.js
+++ js/scripts.js
@@ -1,76 +1,84 @@
 $(document).ready(function () {
-  event.preventDefault();
-  $("#quiz").hide();
-  $(".explaination").hide();
-  $("#startButton").click(function (event) {
-
+  $('#quiz').hide();
+  $("#javascript").hide();
+  $("#python").hide();
+  $("#ruby").hide();
+  $("#startButton").click(function() {
     // Shows the quiz
-    $("#quiz").show();
+    $('#quiz').show();
 
     // when the submit quiz button is pressed - do this
-    $("#btnConfirm").click(function (event) {
+    $("#btnConfirm").click(function () {
       // sets amount of value variables. These will be compared to show whether you should learn JS, Python, or Ruby
       var amountOfA = 0;
       var amountOfB = 0;
       var amountOfC = 0;
 
-      // checks to see value of question one
-      $("input:radio[name=questionOne]:checked").val() {
-        if(value="a") {
-          amountOfA += 1;
-        } else if(value="b") {
-          amountOfB += 1;
-        } else if(value="c") {
-          amountOfC += 1;
-        }
+      var questionone = $("input:radio[name=questionOne]:checked").val();
+      var questiontwo = $("input:radio[name=questionTwo]:checked").val();
+      var questionthree = $("input:radio[name=questionThree]:checked").val();
+      var questionfour = $("input:radio[name=questionFour]:checked").val();
+      var questionfive = $("input:radio[name=questionFive]:checked").val();
+
+      if (questionone === "a") {
+        amountOfA += 1;
+      } else if (questionone === "b") {
+        amountOfB += 1;
+      } else if (questionone === "c") {
+        amountOfC += 1;
+      } else {
+        console.log("There is an error in question one.")
       }
 
-      // checks to see value of question two
-      $("input:radio[name=questionTwo]:checked").val() {
-        if(value="a") {
-          amountOfA += 1;
-        } else if(value="b") {
-          amountOfB += 1;
-        } else if(value="c") {
-          amountOfC += 1;
-        }
+      if (questiontwo === "a") {
+        amountOfA += 1;
+      } else if (questiontwo === "b") {
+        amountOfB += 1;
+      } else if (questiontwo === "c") {
+        amountOfC += 1;
+      } else {
+        console.log("There is an error in question two.")
+      }
 
-      // checks to see value of question Three
-      $("input:radio[name=questionThree]:checked").val() {
-        if(value="a") {
-          amountOfA += 1;
-        } else if(value="b") {
-          amountOfB += 1;
-        } else if(value="c") {
-          amountOfC += 1;
-        }
+      if (questionthree === "a") {
+        amountOfA += 1;
+      } else if (questionthree === "b") {
+        amountOfB += 1;
+      } else if (questionthree === "c") {
+        amountOfC += 1;
+      } else {
+        console.log("There is an error in question three.")
+      }
 
-        // checks to see value of question Four
-      $("input:radio[name=questionFour]:checked").val() {
-        if(value="a") {
-          amountOfA += 1;
-        } else if(value="b") {
-          amountOfB += 1;
-        } else if(value="c") {
-          amountOfC += 1;
-        }
+      if (questionfour === "a") {
+        amountOfA += 1;
+      } else if (questionfour === "b") {
+        amountOfB += 1;
+      } else if (questionfour === "c") {
+        amountOfC += 1;
+      } else {
+        console.log("There is an error in question one.")
+      }
 
-        // checks to see value of question Five
-      $("input:radio[name=questionFive]:checked").val() {
-        if(value="a") {
-          amountOfA += 1;
-        } else if(value="b") {
-          amountOfB += 1;
-        } else if(value="c") {
-          amountOfC += 1;
-        }
+      if (questionfive === "a") {
+        amountOfA += 1;
+      } else if (questionfive === "b") {
+        amountOfB += 1;
+      } else if (questionfive === "c") {
+        amountOfC += 1;
+      } else {
+        console.log("There is an error in question one.")
+      }
 
       if(amountOfA > amountOfB && amountOfA > amountOfC) {
         $("#javascript").show();
+        console.log("JAVASCRIPT");
       } else if(amountOfB > amountOfA && amountOfB > amountOfC) {
         $("#python").show();
+        console.log("PYTHON");
       } else if(amountOfC > amountOfA && amountOfC > amountOfB) {
         $("#ruby").show();
+        console.log("RUBY");
       }
       event.preventDefault();
     });
