2205a83239201006a4d6bf7a389824d19ecdb859
Mikah Mathews <mikah@outofband.org>
2020-03-27 10:38:07 -0700

    It will add hide/show functionality to webpage when start quiz button is pressed.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $("#quiz").hide();
+  $("#startButton").click(function (event) {
+    // Shows the quiz
+    $("quiz").show();
+
+    // when the submit quiz button is pressed - do this
+    $("#btnConfirm").click(function (event) {
+      
+    })
+  })
+})
