f8048182a2ca214b4911f40d766e85865c293006
Mikah Mathews <mikah@outofband.org>
2020-03-27 11:05:29 -0700

    It will add HTML for 'you should learn JS' and start of JS code branching for quiz.

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,9 @@ $(document).ready(function () {
 
     // when the submit quiz button is pressed - do this
     $("#btnConfirm").click(function (event) {
-      
-    })
-  })
-})
+      if $("input:radio[name=quesionOne]:checked") {
+        if 
+      }
+    });
+  });
+});
